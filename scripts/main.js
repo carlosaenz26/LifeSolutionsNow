21aef40ea788831900c01e9f051e776cf9b2fd7b
carlosaenz26 <carlosaenz.26@hotmail.com>
2021-12-26 23:32:24 -0500

    djsk

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 const miTitulo = document.querySelector('h1');
 miTitulo.textContent = '¡Hola mundo!';
-document.querySelector('html').onclick = function() {
+document.querySelector('h1').onclick = function() {
     alert('¡Ouch! ¡Deja de pincharme!');
 }
